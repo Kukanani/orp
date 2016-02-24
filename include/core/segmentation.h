4b9fd371a4980417068e907f88f2f7683437c451
Adam Allevato <allevato@utexas.edu>
2016-02-24 11:40:23 -0600

    Added license files

--- include/core/segmentation.h
+++ include/core/segmentation.h
@@ -1,3 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+//      Title     : sia5-nrg
+//      Project   : NRG ORP
+//      Created   : 1/21/2015
+//      Author    : Adam Allevato
+//      Platforms : Ubuntu 64-bit
+//      Copyright : Copyright© The University of Texas at Austin, 2014-2017. All rights reserved.
+//                 
+//          All files within this directory are subject to the following, unless an alternative
+//          license is explicitly included within the text of each file.
+//
+//          This software and documentation constitute an unpublished work
+//          and contain valuable trade secrets and proprietary information
+//          belonging to the University. None of the foregoing material may be
+//          copied or duplicated or disclosed without the express, written
+//          permission of the University. THE UNIVERSITY EXPRESSLY DISCLAIMS ANY
+//          AND ALL WARRANTIES CONCERNING THIS SOFTWARE AND DOCUMENTATION,
+//          INCLUDING ANY WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//          PARTICULAR PURPOSE, AND WARRANTIES OF PERFORMANCE, AND ANY WARRANTY
+//          THAT MIGHT OTHERWISE ARISE FROM COURSE OF DEALING OR USAGE OF TRADE.
+//          NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH RESPECT TO THE USE OF
+//          THE SOFTWARE OR DOCUMENTATION. Under no circumstances shall the
+//          University be liable for incidental, special, indirect, direct or
+//          consequential damages or loss of profits, interruption of business,
+//          or related expenses which may arise from use of software or documentation,
+//          including but not limited to those resulting from defects in software
+//          and/or documentation, or loss or inaccuracy of data of any kind.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef _SEGMENTATION_H_
 #define _SEGMENTATION_H_
 
@@ -44,8 +74,6 @@ typedef std::vector<pcl::PointIndices> IndexVector;
  * 
  * @author  Brian O'Neil <brian.oneil@lanl.gov>
  * @author  Adam Allevato <adam.d.allevato@gmail.com>
- * @copyright BSD 3-paragraph
- * @date    Jan 21, 2015
  */
 class Segmentation {
 private:
